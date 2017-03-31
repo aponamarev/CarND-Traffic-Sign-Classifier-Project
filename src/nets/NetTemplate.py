2da13262d0c357313f4d217e66fa6175a5aba508
Alex Ponamarev <alex.ponamaryov@gmail.com>
2017-03-30 20:19:29 -0400

    Added activation, batch_norm, and summary layers

--- src/nets/NetTemplate.py
+++ src/nets/NetTemplate.py
@@ -3,29 +3,35 @@ import tensorflow as tf
 import numpy as np
 
 class NetTemplate(object):
-    def __init__(self, input_dict):
+    def __init__(self, input_dict, default_activation='elu', dtype=tf.float32):
         self.inputs = input_dict
         self.weights = []
         self.size = []
+        self._default_activation = default_activation
+        self._dtype = dtype
 
     def get_size(self):
-        return np.sum(np.sum(self.size))
+        params = np.sum(np.sum(self.size))
+        return {'parameters':params, 'Mb': params*self._dtype.size/(2.0**20)}
 
     def define_net(self):
 
         img_batch = self.inputs['X']
 
-        conv = self._conv2d(img_batch, [3,3,3,16], bias=True, name="Layer1")
-        conv2 = self._conv2d(conv, [3,3,16, 64], bias=False, name="Layer2")
-        max_pool = self._max_pool(conv2)
-        avg_pool = self._avg_pool(max_pool)
+        net = self._conv2d(img_batch, [3,3,3,16], bias=True, name="Layer1")
+        net = self._batch_norm(net)
+        net = self._conv2d(net, [3,3,16, 64], bias=False, name="Layer2")
+        net = self._batch_norm(net)
+        net = self._max_pool(net)
+        net = self._batch_norm(net)
+        net = self._avg_pool(net)
+        net = self._batch_norm(net)
+        net = self._fullyconnected(net,10)
 
-        return avg_pool
+        return net
 
-    def _conv2d(self, inputs, shapes, strides=[1,1,1,1], padding="SAME",name="conv2d", bias=True, dtype=tf.float32):
-        # Track stats
-        parameters = []
-        size = []
+    def _conv2d(self, inputs, shapes=[1,3,3,1], strides=[1,1,1,1], padding="SAME",name="conv2d", bias=True, dtype=None):
+        dtype = dtype or self._dtype
         # Create weights and biases
         with tf.variable_scope(name):
             W = tf.get_variable("W",
@@ -33,8 +39,7 @@ class NetTemplate(object):
                                 dtype=dtype,
                                 initializer=tf.contrib.layers.xavier_initializer_conv2d())
 
-            parameters.append(W)
-            size.append(np.prod(shapes, dtype = np.int))
+            self._collect_parameter_stats(W)
 
             conv = tf.nn.conv2d(inputs, W, strides, padding)
 
@@ -43,19 +48,80 @@ class NetTemplate(object):
                 b_init = tf.zeros(b_size, dtype=dtype)
                 b = tf.Variable(b_init, name="b")
                 conv = tf.nn.bias_add(conv, b, data_format='NHWC')
-                parameters.append(b)
-                size.append(b_size)
+                self._collect_parameter_stats(b)
 
-        self.weights.append(parameters)
-        self.size.append(size)
+        return self._activation(conv)
 
-        return conv
+    def _fullyconnected(self, inputs, output_channels, name="fully_connected", bias=True, dtype=None):
+        dtype = dtype or self._dtype
+
+        X = tf.contrib.layers.flatten(inputs)
+        shapes = X.get_shape().as_list()
+        shapes = [shapes[1], output_channels]
+
+        # Create weights and biases
+        with tf.variable_scope(name):
+            W = tf.get_variable("W",
+                                shapes,
+                                dtype=dtype,
+                                initializer=tf.contrib.layers.xavier_initializer())
+
+            self._collect_parameter_stats(W)
+
+            line = tf.matmul(X, W)
+
+            if bias:
+                b_size = shapes[1]
+                b_init = tf.zeros(b_size, dtype=dtype)
+                b = tf.Variable(b_init, name="b")
+
+                self._collect_parameter_stats(b)
+
+                line = tf.nn.bias_add(line, b, data_format='NHWC')
+
+            return self._activation(line)
 
     def _max_pool(self, inputs, kernel=[1,2,2,1], strides=[1,2,2,1], padding="VALID", name = "max_pool"):
         return tf.nn.max_pool(inputs, kernel, strides, padding=padding, name=name)
 
     def _avg_pool(self, inputs, kernel=[1,2,2,1], strides=[1,2,2,1], padding="VALID", name = "max_pool"):
         return tf.nn.avg_pool(inputs, kernel, strides, padding=padding, name=name)
+
+    def _batch_norm(self, input):
+        return tf.layers.batch_normalization(input)
+
+    def _relu_activation(self, input):
+
+        activation = tf.nn.relu(input)
+        tf.summary.histogram('{}_hist'.format(activation.op.name), activation)
+
+        return activation
+
+    def _elu_activation(self, input):
+
+        activation = tf.nn.elu(input)
+        tf.summary.histogram('{}_hist'.format(activation.op.name), activation)
+
+        return activation
+
+    def _activation(self, input, type=None):
+        type = type or self._default_activation
+        implemented_types = {
+            'elu': self._elu_activation,
+            'relu': self._relu_activation
+        }
+        assert type in implemented_types.keys(), "Incorrect type provided ({}). Only {} types are implemented at the moment".\
+            format(type, implemented_types.keys())
+
+        return implemented_types[type](input)
+
+    def _collect_parameter_stats(self, variable):
+        shape = variable.get_shape().as_list()
+        size = np.product(shape, dtype=np.int)
+        tf.summary.histogram('{}_hist'.format(variable.op.name), variable)
+        self.weights.append(variable)
+        self.size.append(size)
+
 
 if __name__ == '__main__':
 
@@ -67,7 +133,7 @@ if __name__ == '__main__':
 
     conv_layer = test_net.define_net()
     print("Model size:", test_net.get_size())
-    shape = conv_layer.get_shape()
+    shape = conv_layer.get_shape().as_list()
     print("Model shape:", shape)
 
     print("test was successful!")
