Fixed buggs around batch normalization and drop out

src/nets/LeNet.py
@@ -2,7 +2,7 @@
 import tensorflow as tf
 from .NetTemplate import NetTemplate
 
-class LeNet_BN(NetTemplate):
+class LeNet(NetTemplate):
     def __init__(self, input_dict):
         img_data = 'img_data'
         labels = 'labels'
@@ -10,7 +10,10 @@ class LeNet_BN(NetTemplate):
             "Incorrect input_dict was provided ({}). Dictionary is expected to contain: {},{}".\
                 format(input_dict.keys(), img_data, labels)
 
-        NetTemplate.__init__(self,default_activation='elu', dtype=tf.float32)
+        NetTemplate.__init__(self,
+                             dropout_rate=tf.constant(1.0, dtype=tf.float32),
+                             training_mode_flag=False,
+                             default_activation='elu', dtype=tf.float32)
 
         self.X = input_dict[img_data]
         self.Y = input_dict[labels]
@@ -31,15 +34,13 @@ class LeNet_BN(NetTemplate):
 
     def define_net(self):
 
-        conv1 = self._conv2d(self.X, [5,5,3,6], bias=False, padding="VALID",name="conv1")
+        conv1 = self._conv2d(self.X, [5,5,3,6], bias=True, padding="VALID",name="conv1")
         pool1 = self._max_pool(conv1, name="pool1")
-        pool1_bn = self._batch_norm(pool1, name="pool1_bn")
 
-        conv2 = self._conv2d(pool1_bn, [5,5, 6, 16], bias=False, padding="VALID", name="conv2")
+        conv2 = self._conv2d(pool1, [5,5, 6, 16], bias=True, padding="VALID", name="conv2")
         pool2 = self._max_pool(conv2, name="pool2")
-        pool2_bn = self._batch_norm(pool2, name="pool2_bn")
 
-        fc3 = self._fullyconnected(pool2_bn, 120, name="fc3")
+        fc3 = self._fullyconnected(pool2, 120, name="fc3")
         fc4 = self._fullyconnected(fc3, 84, name="fc4")
 
         self.feature_map = self._fullyconnected(fc4, self._N_CLASSES, name="feature_map")

src/nets/LeNet_BN_DropOut.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from .NetTemplate import NetTemplate
 
 class LeNet_BN_DropOut(NetTemplate):
-    def __init__(self, input_dict, dropout_placeholder):
+    def __init__(self, input_dict, dropout_placeholder, training_mode_flag):
         img_data = 'img_data'
         labels = 'labels'
         assert (img_data in input_dict.keys()) & (labels in input_dict.keys()),\
@@ -11,9 +11,10 @@ class LeNet_BN_DropOut(NetTemplate):
                 format(input_dict.keys(), img_data, labels)
 
         NetTemplate.__init__(self,
+                             dropout_rate=dropout_placeholder,
+                             training_mode_flag=training_mode_flag,
                              default_activation='elu',
-                             dtype=tf.float32,
-                             dropout_rate=dropout_placeholder)
+                             dtype=tf.float32)
 
         self.X = input_dict[img_data]
         self.Y = input_dict[labels]

src/nets/NetTemplate.py
@@ -3,10 +3,12 @@ import tensorflow as tf
 import numpy as np
 
 class NetTemplate(object):
-    def __init__(self, default_activation='elu', dtype=tf.float32, dropout_rate = tf.constant(0.75, dtype=tf.float32)):
+    def __init__(self, training_mode_flag, dropout_rate, default_activation='elu',
+                 dtype=tf.float32):
         self.weights = []
         self.size = []
         self._dropout_rate = dropout_rate
+        self._training_mode = training_mode_flag
         self._default_activation = default_activation
         self._dtype = dtype
         self.feature_map=None
@@ -91,11 +93,11 @@ class NetTemplate(object):
     def _avg_pool(self, inputs, kernel=[1,2,2,1], strides=[1,2,2,1], padding="VALID", name = "max_pool"):
         return tf.nn.avg_pool(inputs, kernel, strides, padding=padding, name=name)
 
-    def _batch_norm(self, input, name):
-        return tf.layers.batch_normalization(input, name=name)
+    def _batch_norm(self, input, name, trainable=False):
+        return tf.layers.batch_normalization(input, name=name, trainable=trainable, training=self._training_mode)
 
     def _drop_out_fullyconnected(self, input, name):
-        return tf.layers.dropout(input, self._dropout_rate, name=name)
+        return tf.layers.dropout(input, self._dropout_rate, training=self._training_mode, name=name)
 
     def _drop_out_conv(self, input, name):
         shape = input.get_shape().as_list()

src/nets/SmallFilters.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from .NetTemplate import NetTemplate
 
 class SmallFilters(NetTemplate):
-    def __init__(self, input_dict, dropout_placeholder):
+    def __init__(self, input_dict, dropout_placeholder, training_mode_flag):
         img_data = 'img_data'
         labels = 'labels'
         assert (img_data in input_dict.keys()) & (labels in input_dict.keys()),\
@@ -11,9 +11,10 @@ class SmallFilters(NetTemplate):
                 format(input_dict.keys(), img_data, labels)
 
         NetTemplate.__init__(self,
+                             dropout_rate = dropout_placeholder,
+                             training_mode_flag = training_mode_flag,
                              default_activation='elu',
-                             dtype=tf.float32,
-                             dropout_rate=dropout_placeholder)
+                             dtype=tf.float32)
 
         self.X = input_dict[img_data]
         self.Y = input_dict[labels]
@@ -39,19 +40,19 @@ class SmallFilters(NetTemplate):
         bottleneck3 = self._conv2d(conv2, [1, 1, 16, 4], bias=True, padding="SAME", name="bottleneck3")
         bn3 = self._batch_norm(bottleneck3, name="bn3") #16
 
-        conv4 = self._conv2d(bn3, [3, 3, 4, 16], bias=False, padding="SAME", name="conv4")
-        conv5 = self._conv2d(conv4, [3, 3, 16, 32], strides=[1, 2, 2, 1], bias=True, padding="VALID", name="conv5")
-        bottleneck6 = self._conv2d(conv5, [1, 1, 32, 16], bias=True, padding="SAME", name="bottleneck6")
+        conv4 = self._conv2d(bn3, [3, 3, 4, 8], bias=False, padding="SAME", name="conv4")
+        conv5 = self._conv2d(conv4, [3, 3, 8, 16], strides=[1, 2, 2, 1], bias=True, padding="VALID", name="conv5")
+        bottleneck6 = self._conv2d(conv5, [1, 1, 16, 4], bias=True, padding="SAME", name="bottleneck6")
         bn6 = self._batch_norm(bottleneck6, name="bn6") #8
 
         dropout = self._drop_out_conv(bn6, "dropout_layer6")
 
-        conv7 = self._conv2d(dropout, [3, 3, 16, 64], strides=[1, 2, 2, 1], bias=False, padding="SAME", name="conv7") #4
-        conv8 = self._conv2d(conv7, [3, 3, 64, 128], bias=True, padding="VALID", name="conv8") #2
+        conv7 = self._conv2d(dropout, [3, 3, 4, 120], strides=[1, 2, 2, 1], bias=False, padding="SAME", name="conv7") #4
+        conv8 = self._conv2d(conv7, [3, 3, 120, 84], bias=True, padding="VALID", name="conv8") #2
         with tf.name_scope("feature_map"):
             self.feature_map = tf.squeeze(
                 self._conv2d(conv8,
-                             [2, 2, 128, self._N_CLASSES],
+                             [2, 2, 84, self._N_CLASSES],
                              bias=True,
                              padding="VALID",name="conv"))
 