f473f6cd933f5bd4ade51a835c858d88d6c33acb
Alex Ponamarev <alex.ponamaryov@gmail.com>
2017-04-04 22:58:40 -0400

    Update ClassificationTemplate to incorporate gpu support

--- src/nets/ClassificationTemplate.py
+++ src/nets/ClassificationTemplate.py
@@ -1,6 +1,5 @@
 # Author: Alexander Ponamarev (alex.ponamaryov@gmail.com) 04/30/2017
 import tensorflow as tf
-import tflearn
 from .NetTemplate import NetTemplate
 
 class ClassificationTemplate(NetTemplate):
@@ -50,41 +49,44 @@ class ClassificationTemplate(NetTemplate):
         #     total_loss = control_flow_ops.with_dependencies([updates], total_loss)
         # Reference: http://ruishu.io/2016/12/27/batchnorm/
 
-        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.device('/gpu:0'):
+            update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
 
-        if self.pdf is None:
+            if self.pdf is None:
 
-            with tf.control_dependencies(update_ops):
-                # Ensures that we execute the update_ops before performing the train_step
-                with tf.name_scope("cross_entropy_loss"):
+                with tf.control_dependencies(update_ops):
+                    # Ensures that we execute the update_ops before performing the train_step
+                    with tf.name_scope("cross_entropy_loss"):
 
-                    self.total_loss = tf.reduce_mean(
-                        tf.nn.softmax_cross_entropy_with_logits(labels=self.Y, logits=self.feature_map)
-                    )
-        else:
+                        self.total_loss = tf.reduce_mean(
+                            tf.nn.softmax_cross_entropy_with_logits(labels=self.Y, logits=self.feature_map)
+                        )
+            else:
 
-            with tf.control_dependencies(update_ops):
-                # Ensures that we execute the update_ops before performing the train_step
-                with tf.name_scope("cross_entropy_loss"):
+                with tf.control_dependencies(update_ops):
+                    # Ensures that we execute the update_ops before performing the train_step
+                    with tf.name_scope("cross_entropy_loss"):
 
-                    P_of_x = tf.nn.softmax(logits=self.feature_map)
-                    P_of_x_given_PDF = tf.divide(P_of_x, self.pdf)
+                        P_of_x = tf.nn.softmax(logits=self.feature_map)
+                        P_of_x_given_PDF = tf.divide(P_of_x, self.pdf)
 
-                    self.total_loss = tf.reduce_mean(
-                        tf.nn.softmax_cross_entropy_with_logits(labels=self.Y, logits=P_of_x_given_PDF)
-                    )
+                        self.total_loss = tf.reduce_mean(
+                            tf.nn.softmax_cross_entropy_with_logits(labels=self.Y, logits=P_of_x_given_PDF)
+                        )
 
     def _define_prediction(self):
         assert self.feature_map is not None, "Error: Feature map wasn't defined."
-        with tf.name_scope("class_prediction"):
-            self.probability_op = tf.nn.softmax(self.feature_map, name="probability")
-            self.predict_class_op = tf.arg_max(self.probability_op, 1, name="label")
+        with tf.device('/gpu:0'):
+            with tf.name_scope("class_prediction"):
+                self.probability_op = tf.nn.softmax(self.feature_map, name="probability")
+                self.predict_class_op = tf.arg_max(self.probability_op, 1, name="label")
 
     def _define_accuracy(self):
-        self.accuracy_op = tf.reduce_mean(
-            tf.cast(tf.equal(self.predict_class_op, tf.arg_max(self.Y, 1, name="predict_accuracy")),
-                    dtype=tf.float32)
-        )
+        with tf.device('/gpu:0'):
+            self.accuracy_op = tf.reduce_mean(
+                tf.cast(tf.equal(self.predict_class_op, tf.arg_max(self.Y, 1, name="predict_accuracy")),
+                        dtype=tf.float32)
+            )
 
     def eval(self, X_batch, Y_batch):
         sess = tf.get_default_session()
